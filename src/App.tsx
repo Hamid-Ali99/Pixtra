3abcdcabd1d14646f321a96267617fb81cba3af6
Hamid <hamidali006789@gmail.com>
2024-07-07 07:29:51 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+function App() {
+  return (
+    <>
+      My App
+      <h1 className="text-3xl font-bold text-purple-600 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
